6cd8047c1e33b79c1ea383d2d85e9bbf89f2fa14
Michtz <105941997+Michtz@users.noreply.github.com>
2024-12-30 13:33:15 +0100

    integrated prettier

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,19 +1,33 @@
 import { Router, Response } from 'express';
-import {createOrder, getOrderById, getUserOrders, updateOrderStatus} from "../controllers/order.controller";
-import { OrderRequest} from "../models/order.model";
+import {
+  allOrders,
+  createOrder,
+  getOrderById,
+  getUserOrders,
+  updateOrderStatus,
+} from '../controllers/order.controller';
+import { OrderRequest } from '../models/order.model';
 
 const router = Router();
 
-router.route('/:userId')
-    .post((req: OrderRequest, res: Response) => createOrder(req, res))
+router
+  .route('/')
+  .get((req: OrderRequest, res: Response) => allOrders(req, res));
 
-router.route('/user/:userId')
-    .post((req: OrderRequest, res: Response) => getUserOrders(req, res))
+router
+  .route('/:userId')
+  .post((req: OrderRequest, res: Response) => createOrder(req, res));
 
-router.route('/:orderId')
-    .post((req: OrderRequest, res: Response) => getOrderById(req, res))
+router
+  .route('/user/:userId')
+  .get((req: OrderRequest, res: Response) => getUserOrders(req, res));
 
-router.route('/:orderId/status')
-    .post((req: OrderRequest, res: Response) => updateOrderStatus(req, res))
+router
+  .route('/:orderId')
+  .get((req: OrderRequest, res: Response) => getOrderById(req, res));
+
+router
+  .route('/:orderId/status')
+  .patch((req: OrderRequest, res: Response) => updateOrderStatus(req, res));
 
 export default router;
